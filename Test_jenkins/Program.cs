9472fcee7962ca6aebb8e68160cf60588111cde5
AkshadaBhambure <akshadabhambure1011@gmail.com>
2025-07-25 12:20:19 +0530

    Add project files.

---
+++ Test_jenkins/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_jenkins
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+            int a=10, b=20;
+            int c = a + b;
+            Console.WriteLine("Addition:",c);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Addition();
+        }
+    }
+}
